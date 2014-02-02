930970491ad4b003498aebc4ee64c5f5ceb2c41d
Kyle Neumeier <kneumei@gmail.com>
2014-02-02 16:04:55 -0600

    Retool. Now use lineman

---
+++ config/files.js
@@ -0,0 +1,37 @@
+/* Exports an object that defines
+ *  all of the paths & globs that the project
+ *  is concerned with.
+ *
+ * The "configure" task will require this file and
+ *  then re-initialize the grunt config such that
+ *  directives like <config:files.js.app> will work
+ *  regardless of the point you're at in the build
+ *  lifecycle.
+ *
+ * You can find the parent object in: node_modules/lineman/config/files.coffee
+ */
+
+module.exports = require(process.env['LINEMAN_MAIN']).config.extend('files', {
+  js: {
+    vendor: [
+      "vendor/bower/angular/angular.js",
+      "vendor/bower/angular-route/angular-route.js",
+      "vendor/bower/angular-resource/angular-resource.js",
+      "vendor/bower/lodash/lodash.js",
+      "vendor/bower/jquery/jquery.js",
+      "vendor/js/**/*.js"
+    ],
+    app: [
+      "app/js/app.js",
+      "app/js/**/*.js"
+    ]
+  },
+
+  less: {
+    vendor:[
+    "vendor/css/normalize.css", 
+    "vendor/css/**/*.css",
+    "vendor/bower/bootstrap/less/bootstrap.less"
+    ]
+  }
+});
--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-function crdCtrl($scope){
-		$scope.items=[
-			{name:"ABC", comment:"comment"},
-			{name:"Name1", comment:"comment2"}
-		]
-	}
